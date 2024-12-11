8752cf7294b59bd3644da87c9d1db8cd7aa05701
soulchicken <soulfever01@naver.com>
2024-12-11 20:33:26 +0900

    feat: scroll down 버튼 에니메이션

--- frontend/components/icons/ScrollDownArrow.tsx
+++ frontend/components/icons/ScrollDownArrow.tsx
@@ -2,8 +2,33 @@ import Image from "next/image";
 
 import ScrollDown from "@/assets/icons/scroll-down.svg";
 
+const scrollStyle = {
+  animation: "fadeInOut 2s infinite",
+};
+
+const keyframes = `
+  @keyframes fadeInOut {
+    0%, 100% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+  }
+`;
+
 const ScrollDownArrow = () => {
-  return <Image alt="Right Arrow Icon" src={ScrollDown} width={60} />;
+  return (
+    <>
+      <style>{keyframes}</style>
+      <Image
+        alt="Right Arrow Icon"
+        src={ScrollDown}
+        style={scrollStyle}
+        width={60}
+      />
+    </>
+  );
 };
 
 export default ScrollDownArrow;
