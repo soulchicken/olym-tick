5ace1ca54508f59a4b09e6212e81907c881b7e2f
soulchicken <soulfever01@naver.com>
2024-12-13 04:37:10 +0900

    chore: search console

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
-import Head from "next/head";
 import React from "react";
 
 import NavBar from "@/components/common/NavBar";
@@ -10,18 +9,15 @@ import { css } from "@/styled-system/css";
 export const metadata: Metadata = {
   title: "OYLM-TICK",
   description: "올림픽의 기억을 되새기다",
+  verification: {
+    google: "JgLLDfBTg42CaqHoNuJqRjhpDmQafWLgtjGOzETwz2Q",
+  },
 };
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <>
       <html lang="ko">
-        <Head>
-          <meta
-            content="JgLLDfBTg42CaqHoNuJqRjhpDmQafWLgtjGOzETwz2Q"
-            name="google-site-verification"
-          />
-        </Head>
         <body className={defaultStyle}>
           <NavBar />
           {children}
