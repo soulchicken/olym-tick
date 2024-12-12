4184f0114fa3c8aa2b314adce92aa11e372a01ba
soulchicken <soulfever01@naver.com>
2024-12-13 04:32:34 +0900

    feat: olym-tick 진입 페이지

--- frontend/app/olym-tick/page.tsx
+++ frontend/app/olym-tick/page.tsx
@@ -7,94 +7,118 @@ import MemoryImg from "@/assets/images/memory_img.png";
 import RemindImg from "@/assets/images/remind_img.png";
 import RoundBox from "@/components/common/RoundBox";
 import Text from "@/components/common/Text";
-import useScrollAnimation from "@/hooks/useScrollAnimation";
 import logoImg from "@/public/white-logo.png";
 import { css } from "@/styled-system/css";
 
 const page = () => {
-  const { isVisible, domRef } = useScrollAnimation();
-
   return (
-    <main
-      className={mainStyle}
-      ref={domRef}
-      style={{
-        opacity: isVisible ? 1 : 0,
-        transition: "opacity 2s",
-      }}
-    >
-      <div>
-        <Image
-          alt="logo"
-          height={106}
-          src={logoImg}
-          style={logoStyle}
-          width={233}
-        />
-        <Text size="2.25rem" weight={700}>
-          올림픽의 기억을 담는&nbsp;
-          <Text as="span" color="secondary" size="2.25rem" weight={700}>
-            티켓
+    <>
+      <style>{keyframes}</style>
+      <main
+        className={mainStyle}
+        style={{
+          animation: "fadeIn 2s ease-in-out forwards",
+          opacity: 0, // 초기 투명도 설정
+        }}
+      >
+        <div>
+          <Image
+            alt="logo"
+            height={106}
+            src={logoImg}
+            style={logoStyle}
+            width={233}
+          />
+          <Text size="2.25rem" weight={700}>
+            올림픽의 기억을 담는&nbsp;
+            <Text as="span" color="secondary" size="2.25rem" weight={700}>
+              티켓
+            </Text>
           </Text>
-        </Text>
-        <Text
-          as="h1"
-          className={css({ letterSpacing: "0.075rem", lineHeight: "6.25rem" })}
-          font="headline"
-          size="7.5rem"
-        >
-          Oylm-Tick
-        </Text>
-        <Text>
-          올림픽 티켓으로 당신의 관람 기억을 남겨요.
-          <br />
-          관람 당시의 영상기록을 보내주세요.
-          <br />
-          현장감을 다시 느끼게 도와드릴게요.
-        </Text>
-      </div>
-      <div style={gridStyle}>
-        <RoundBox isHover>
-          <Link href="/olym-tick/remind">
+          <Text
+            as="h1"
+            font="headline"
+            size="7.5rem"
+            className={css({
+              letterSpacing: "0.075rem",
+              lineHeight: "6.25rem",
+            })}
+          >
+            Oylm-Tick
+          </Text>
+          <Text>
+            올림픽 티켓으로 당신의 관람{" "}
+            <Text as="span" weight="bold">
+              기억
+            </Text>
+            을 남겨요.
+            <br />
+            관람
+            <Text as="span" weight="bold">
+              당시의 영상기록
+            </Text>
+            을 보내주세요.
+            <br />
+            <Text as="span" weight="bold">
+              현장감
+            </Text>
+            을 다시 느끼게 도와드릴게요.
+          </Text>
+        </div>
+        <div style={gridStyle}>
+          <RoundBox isHover>
+            <Link href="/olym-tick/remind">
+              <div style={wrapperStyle}>
+                <Image alt="remind" src={RemindImg} width={180} />
+                <div className={textBoxStyle}>
+                  <Text className={subTextStyle}>
+                    올림픽을 보고 들었던 <br /> 현장의 기억을 돌아봐요.
+                  </Text>
+                  <Text as="h2" size="2rem" weight={700}>
+                    <Text as="span" color="secondary" size="2rem" weight={700}>
+                      회상
+                    </Text>
+                    하기
+                  </Text>
+                </div>
+              </div>
+            </Link>
+          </RoundBox>
+          <RoundBox isHover>
             <div style={wrapperStyle}>
-              <Image alt="remind" src={RemindImg} width={180} />
+              <Image alt="memory" src={MemoryImg} width={100} />
               <div className={textBoxStyle}>
                 <Text className={subTextStyle}>
-                  올림픽을 보고 들었던 <br /> 현장의 기억을 돌아봐요.
+                  올림픽 현장의 기억을
+                  <br /> 보관해드릴게요.
                 </Text>
                 <Text as="h2" size="2rem" weight={700}>
                   <Text as="span" color="secondary" size="2rem" weight={700}>
-                    회상
+                    기억
                   </Text>
-                  하기
+                  만들기
                 </Text>
               </div>
             </div>
-          </Link>
-        </RoundBox>
-        <RoundBox isHover>
-          <div style={wrapperStyle}>
-            <Image alt="memory" src={MemoryImg} width={100} />
-            <div className={textBoxStyle}>
-              <Text className={subTextStyle}>
-                올림픽 현장의 기억을
-                <br /> 보관해드릴게요.
-              </Text>
-              <Text as="h2" size="2rem" weight={700}>
-                <Text as="span" color="secondary" size="2rem" weight={700}>
-                  기억
-                </Text>
-                만들기
-              </Text>
-            </div>
-          </div>
-        </RoundBox>
-      </div>
-    </main>
+          </RoundBox>
+        </div>
+      </main>
+    </>
   );
 };
 
 export default page;
+
+const keyframes = `
+  @keyframes fadeIn {
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+}
+`;
 
 const mainStyle = css({
   padding: "1rem",
